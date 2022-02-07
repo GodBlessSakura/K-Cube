7d13de25374913a74b57ffb2c632cdd552f20f68
jerry <14075364d@connect.polyu.hk>
2022-02-07 13:47:43 +0800

    add permission check and update url of dashboard

--- app/blueprints/operator/__init__.py
+++ app/blueprints/operator/__init__.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, render_template, abort
+from app.authorizer import authorize_with
 
 operator = Blueprint("operator", __name__, template_folder="templates")
 
+@operator.before_request
+@authorize_with([], True, ["student", "admin"])
+def middleware():
+    pass
 
-@operator.route("/")
+@operator.route("/dashboard")
 def dashboard():
     return render_template("operator/dashboard.html")
--- app/blueprints/student/__init__.py
+++ app/blueprints/student/__init__.py
@@ -1,8 +1,14 @@
 from flask import Blueprint, render_template, abort
+from app.authorizer import authorize_with
 
 student = Blueprint("student", __name__, template_folder="templates")
 
 
-@student.route("/")
+@student.before_request
+@authorize_with([], True, ["student", "admin"])
+def middleware():
+    pass
+
+@student.route("/dashboard")
 def dashboard():
     return render_template("student/dashboard.html")
