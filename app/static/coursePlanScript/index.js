0035f48111df7b64d84fa5f41a02189489353660
jerry <14075364d@connect.polyu.hk>
2022-03-31 19:52:51 +0800

    new course plan

--- app/static/coursePlanScript/index.js
+++ app/static/coursePlanScript/index.js
@@ -5,7 +5,7 @@ var olddata = {
 };
 var newdata = ["new1", "new2"];
 
-function assignWeekValue(triples, activities, courseCode) {
+function graph_traversal(triples, activities, courseCode) {
     let firstHop =
         triples.filter(triple => triple.h_name == courseCode || triple.t_name == courseCode)
         .map(triple => {
@@ -21,11 +21,7 @@ function assignWeekValue(triples, activities, courseCode) {
         }
     }
     activities.forEach(activity => {
-        oldData[activity.name] = {
-            uri: activity.name,
-            week: activity.week,
-            desc: activity.desc
-        }
+        oldData[activity.name] = createItem(activity.desc, activity.name, activity.week)
     });
     let visited = [courseCode, ...firstHop]
 
@@ -57,12 +53,6 @@ function assignWeekValue(triples, activities, courseCode) {
             if (candidate.length == 0) {
                 return true
             } else {
-                let activity = candidate[0]
-                oldData[candidate[0].name] = {
-                    uri: candidate[0].name,
-                    week: candidate[0].week,
-                    desc: candidate[0].desc
-                }
                 return false
             }
         })
@@ -70,38 +60,22 @@ function assignWeekValue(triples, activities, courseCode) {
     return [newData, oldData]
 }
 
-function createDiv(content, uri) {
-    var createDiv = document.createElement("div");
-    createDiv.className = "list";
-    createDiv.draggable = "true";
-    createDiv.contentEditable = "true";
-    createDiv.innerHTML = content;
-    createDiv.dataset.uri = uri;
-    return createDiv;
-}
 
-function constructTimetable(triples, activities, courseCode) {
+function assign_timeslot(triples, activities, courseCode) {
     let [newdata,
         olddata
-    ] = assignWeekValue(triples, activities, courseCode)
+    ] = graph_traversal(triples, activities, courseCode)
     console.log(newdata)
     console.log(olddata)
     let timeslots = {}
     for (let i = 1; i < 14; i++) {
-        timeslots[i + ''] = document.getElementById('week' + i);
-    }
-    for (var i = 0; i <= 12; i++) {
-
-        timeslots[i + '.5'] = document.getElementById('week' + i + '.5');
-    }
-    for (id in timeslots) {
-        timeslots[id].innerHTML = id % 1 != 0 ? '' : '<div class="time" draggable="false">Week ' + id + '</div>'
+        timeslots[i - .5] = [];
+        timeslots[i + ''] = [];
     }
     for (uri in olddata) {
         if (olddata[uri].week < 0) continue;
-        let timeSlot = document.getElementById('week' + olddata[uri].week);
-        let entity = createDiv(olddata[uri]["desc"], uri);
-        timeSlot.appendChild(entity);
+        let timeSlot = timeslots[olddata[uri].week];
+        if (timeSlot) timeSlot.push(olddata[uri]);
     }
     // add new data
     let totalItem = 0
@@ -126,16 +100,18 @@ function constructTimetable(triples, activities, courseCode) {
                         (totalItem / 13)) - 1,
                 )
                 slot = slot < 0 ? 0 : slot
-                let timeSlot = timeslots[slot + 1];
-                entity = createDiv(content, uri);
-                timeSlot.appendChild(entity);
+                slot++
+                let timeSlot = timeslots[slot];
+                entity = createItem(content, uri, undefined);
+                timeSlot.push(entity);
                 counter++
             }
             let uri = firstHopEntity,
                 content = firstHopEntity;
-            let timeSlot = timeslots[slot_cursor + '.5'];
-            entity = createDiv(content, uri);
-            timeSlot.appendChild(entity);
+            let slot = slot_cursor + '.5'
+            let timeSlot = timeslots[slot];
+            entity = createItem(content, uri, undefined);
+            timeSlot.push(entity);
         } else {}
     }
     for (firstHopEntity in newdata) {
@@ -144,116 +120,22 @@ function constructTimetable(triples, activities, courseCode) {
         } else if (newdata[firstHopEntity].length > 0) {} else {
             let uri = firstHopEntity,
                 content = firstHopEntity;
-            let slot = Math.floor(counter / (totalItem / 13))
-            let timeSlot = timeslots[slot + 1];
-            entity = createDiv(content, uri);
-            timeSlot.appendChild(entity);
+            let slot = Math.floor(counter / (totalItem / 13)) + 1
+            let timeSlot = timeslots[slot];
+            entity = createItem(content, uri, undefined);
+            timeSlot.push(entity);
             counter++
         }
     }
-
-    /* blank for add */
-    for (var i = 1; i <= 13; i++) {
-        timeSlot = document.getElementById('week' + i);
-        entity = createDiv("", "");
-        entity.draggable = false
-        timeSlot.appendChild(entity);
-    }
-    for (var i = 0; i <= 12; i++) {
-        timeSlot = document.getElementById('week' + i + '.5');
-        entity = createDiv("", "");
-        entity.draggable = false
-        timeSlot.appendChild(entity);
-    }
-
-    var iosDragDropShim = {
-        enableEnterLeave: true
-    };
-    var source = document.querySelectorAll('.list'),
-        recycle = document.getElementById('recycle'),
-        dragElement = null,
-        lock = true;
-
-    for (var i = 0; i < source.length; i++) {
-        source[i].addEventListener('dragstart', function (ev) {
-            dragElement = this;
-            ev.target.style = ev.target.style ? ev.target.style : {}
-            ev.target.style["back-groundColor"] = '#f8f8f8';
-        }, false);
-
-        source[i].addEventListener('dragend', function (ev) {
-            ev.target.style = ev.target.style ? ev.target.style : {}
-            ev.target.style["back-groundColor"] = '#fff';
-            ev.preventDefault();
-        }, false)
-
-        source[i].addEventListener('dragenter', function (ev) {
-            if (dragElement != this) {
-                this.parentNode.insertBefore(dragElement, this);
-            }
-        }, false)
-
-        source[i].addEventListener('dragleave', function (ev) {
-            console.log(111);
-            if (dragElement != this) {
-                if (lock && (this == this.parentNode.lastElementchildren || this == this.parentNode.lastchildren)) {
-                    this.parentNode.appendChild(dragElement);
-                    lock = false;
-                } else {
-                    lock = true;
-                }
-            }
-        }, false)
-    };
-    recycle.addEventListener('drop', function (ev) {
-        dragElement.parentNode.removechildren(dragElement);
-    }, false)
-
-    document.ondragover = function (e) {
-        e.preventDefault();
-    }
-    document.ondrop = function (e) {
-        e.preventDefault();
-    }
-
-
-    document.querySelector('#upJS').addEventListener('click', uploadTeachplan)
-
+    return timeslots
 }
 
 
-function createItem(div, day) {
+function createItem(content, uri, week) {
     var item = {
-        "name": "",
-        "desc": "",
-        "week": 0,
+        "name": uri,
+        "desc": content,
+        "week": week
     };
-    item["name"] = div.dataset.uri;
-    item["desc"] = div.innerHTML;
-    item["week"] = day;
     return item;
-}
-
-function uploadTeachplan() {
-    let timeslots = {}
-    for (let i = 1; i < 14; i++) {
-
-        timeslots[i] = document.getElementById('week' + i);
-    }
-    for (var i = 0; i <= 12; i++) {
-
-        timeslots[i + '.5'] = document.getElementById('week' + i + '.5');
-    }
-    console.log(timeslots)
-    for (day in timeslots) {
-        var weeklyDivlist = timeslots[day].querySelectorAll('.list');
-        var item = null;
-        for (let i = 0; i < weeklyDivlist.length; i++) {
-            if (weeklyDivlist[i].innerHTML) {
-                item = createItem(weeklyDivlist[i], day);
-                updateItem(item);
-            }
-
-        }
-    }
 }
